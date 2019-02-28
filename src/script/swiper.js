6f6e2b071a8733e1574267ba5cb2e03fac695de9
OldBigBuddha <development@oldbigbuddha.net>
2019-02-28 15:35:09 +0200

    [update] swiper config
    enable keyboard
    enable mousewheel

--- src/script/swiper.js
+++ src/script/swiper.js
@@ -1,13 +1,20 @@
-const swiper = new Swiper ('.swiper-container',{
-  // Optional parameters
-  direction: 'vertical',
-  loop: false,
-  pagination: {
-    // If we need pagination
-    el: '.swiper-pagination'
-  },
-  scrollbar: {
-    // And if we need scrollbar
-    el: '.swiper-scrollbar'
-  }
-})
+window.onload = () => {
+  const swiper = new Swiper ('.swiper-container',{
+    // Optional parameters
+    direction: 'vertical',
+    loop: false,
+    pagination: {
+      // If we need pagination
+      el: '.swiper-pagination'
+    },
+    scrollbar: {
+      // And if we need scrollbar
+      el: '.swiper-scrollbar'
+    },
+    keyboard: {
+      enable: true,
+      onlyInViewport: false
+    },
+    mousewheel: true
+  })
+}
